3a544ee936c279e6582db23d54f6a1a2c9444cca
jeremyBass <jeremybass@cableone.net>
2014-03-14 08:56:14 -0700

    adds the data with config pulls/ reformats just to be safe

--- app/code/community/Wsu/ComingSoon/Controller/Router/Standard.php
+++ app/code/community/Wsu/ComingSoon/Controller/Router/Standard.php
@@ -1,98 +1,98 @@
 <?php
 class Wsu_ComingSoon_Controller_Router_Standard extends Mage_Core_Controller_Varien_Router_Standard {
 
-    public function match(Zend_Controller_Request_Http $request) {
+	public function match(Zend_Controller_Request_Http $request) {
 
-        $helper = Mage::helper('Wsu_ComingSoon');
-        $storeCode = $request->getStoreCodeFromPath();
+		$helper = Mage::helper('Wsu_ComingSoon');
+		$storeCode = $request->getStoreCodeFromPath();
 
-        $enabled = $helper->getConfig('enabled', $storeCode);
+		$enabled = $helper->getConfig('enabled', $storeCode);
 
-        // module enabled?
-        if (1 == $enabled) {
+		// module enabled?
+		if (1 == $enabled) {
 
-            $allowedIPsString = $helper->getConfig('allowedIPs', $storeCode);
+			$allowedIPsString = $helper->getConfig('allowedIPs', $storeCode);
 
-            // remove spaces from string
-            $allowedIPsString = preg_replace('/ /', '', $allowedIPsString);
+			// remove spaces from string
+			$allowedIPsString = preg_replace('/ /', '', $allowedIPsString);
 
-            $allowedIPs = array();
+			$allowedIPs = array();
 
-            if ('' !== trim($allowedIPsString)) {
-                $allowedIPs = explode(',', $allowedIPsString);
-            }
+			if ('' !== trim($allowedIPsString)) {
+				$allowedIPs = explode(',', $allowedIPsString);
+			}
 
-            $currentIP = $_SERVER['REMOTE_ADDR'];
+			$currentIP = $_SERVER['REMOTE_ADDR'];
 
-            $allowFrontendForAdmins = $helper->getConfig('allowFrontendForAdmins', $storeCode);
+			$allowFrontendForAdmins = $helper->getConfig('allowFrontendForAdmins', $storeCode);
 
-            $adminIp = null;
-            if (1 == $allowFrontendForAdmins) {
-                //get the admin session
-                Mage::getSingleton('core/session', array('name' => 'adminhtml'));
+			$adminIp = null;
+			if (1 == $allowFrontendForAdmins) {
+				//get the admin session
+				Mage::getSingleton('core/session', array('name' => 'adminhtml'));
 
 
-                //verify if the user is logged in to the backend
-                $adminSession = Mage::getSingleton('admin/session');
-                if ($adminSession->isLoggedIn()) {
-                    //do stuff
-                    $adminIp = $adminSession['_session_validator_data']['remote_addr'];
-                }
-            }
+				//verify if the user is logged in to the backend
+				$adminSession = Mage::getSingleton('admin/session');
+				if ($adminSession->isLoggedIn()) {
+					//do stuff
+					$adminIp = $adminSession['_session_validator_data']['remote_addr'];
+				}
+			}
 
-            if ($currentIP === $adminIp) {
-                // current user is logged in as admin
-                $this->__log('Access granted for admin with IP: ' . $currentIP . ' and store ' . $storeCode, 2, $storeCode);
-            } else {
-                // current user allowed to access website?
-                if (!in_array($currentIP, $allowedIPs)) {
-                    $this->__log('Access denied  for IP: ' . $currentIP . ' and store ' . $storeCode, 1, $storeCode);
+			if ($currentIP === $adminIp) {
+				// current user is logged in as admin
+				$this->__log('Access granted for admin with IP: ' . $currentIP . ' and store ' . $storeCode, 2, $storeCode);
+			} else {
+				// current user allowed to access website?
+				if (!in_array($currentIP, $allowedIPs)) {
+					$this->__log('Access denied  for IP: ' . $currentIP . ' and store ' . $storeCode, 1, $storeCode);
 
-                    $maintenancePage = trim($helper->getConfig('maintenancePage', $storeCode));
-                    // if custom maintenance page is defined in backend, display this one
-                    if ('' !== $maintenancePage) {
+					$maintenancePage = trim($helper->getConfig('maintenancePage', $storeCode));
+					// if custom maintenance page is defined in backend, display this one
+					if ('' !== $maintenancePage) {
 
-                        Mage::getSingleton('core/session', array('name' => 'front'));
+						Mage::getSingleton('core/session', array('name' => 'front'));
 
-                        $response = $this->getFront()->getResponse();
+						$response = $this->getFront()->getResponse();
 
-                        $response->setHeader('HTTP/1.1', '503 Service Temporarily Unavailable');
-                        $response->setHeader('Status', '503 Service Temporarily Unavailable');
-                        $response->setHeader('Retry-After', '5000');
+						$response->setHeader('HTTP/1.1', '503 Service Temporarily Unavailable');
+						$response->setHeader('Status', '503 Service Temporarily Unavailable');
+						$response->setHeader('Retry-After', '5000');
 
-                        $response->setBody($maintenancePage);
-                        $response->sendHeaders();
-                        $response->outputBody();
-                    }
-                    exit();
-                } else {
-                    $this->__log('Access granted for IP: ' . $currentIP . ' and store ' . $storeCode, 2, $storeCode);
-                }
-            }
-        }
+						$response->setBody($maintenancePage);
+						$response->sendHeaders();
+						$response->outputBody();
+					}
+					exit();
+				} else {
+					$this->__log('Access granted for IP: ' . $currentIP . ' and store ' . $storeCode, 2, $storeCode);
+				}
+			}
+		}
 
-        return parent::match($request);
-    }
+		return parent::match($request);
+	}
 
-    /**
-     * logging helper
-     * 
-     * @param type $string      text to log
-     * @param type $zendLevel   Zend_Log logging level
-     * @param type $verbosityLevelRequired verbosity (0 = no logging, 1 = only denied requests, 2 = denied and granted requests)
-     */
-    private function __log($string, $verbosityLevelRequired = 1, $storeCode = null, $zendLevel = Zend_Log::DEBUG) {
-        $helper = Mage::helper('Wsu_ComingSoon');
-        $logFile = trim($helper->getConfig('logFile', $storeCode));
-        $logVerbosity = trim($helper->getConfig('logVerbosity', $storeCode));
+	/**
+	 * logging helper
+	 * 
+	 * @param type $string      text to log
+	 * @param type $zendLevel   Zend_Log logging level
+	 * @param type $verbosityLevelRequired verbosity (0 = no logging, 1 = only denied requests, 2 = denied and granted requests)
+	 */
+	private function __log($string, $verbosityLevelRequired = 1, $storeCode = null, $zendLevel = Zend_Log::DEBUG) {
+		$helper = Mage::helper('Wsu_ComingSoon');
+		$logFile = trim($helper->getConfig('logFile', $storeCode));
+		$logVerbosity = trim($helper->getConfig('logVerbosity', $storeCode));
 
-        if ('' === $logFile) {
-            $logFile = 'maintenance.log';
-        }
+		if ('' === $logFile) {
+			$logFile = 'maintenance.log';
+		}
 
-        if ($logVerbosity >= $verbosityLevelRequired) {
-            Mage::log($string, $zendLevel, $logFile);
-        }
-    }
+		if ($logVerbosity >= $verbosityLevelRequired) {
+			Mage::log($string, $zendLevel, $logFile);
+		}
+	}
 
 }
