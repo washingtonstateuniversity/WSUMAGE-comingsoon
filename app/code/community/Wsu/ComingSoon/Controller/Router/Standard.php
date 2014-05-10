356ee68eef800ffb0d149dfe9e2c475300bb4396
Jeremy Bass <jeremybass@cableone.net>
2014-05-10 16:29:26 -0700

    skip mages way of redirect

--- app/code/community/Wsu/ComingSoon/Controller/Router/Standard.php
+++ app/code/community/Wsu/ComingSoon/Controller/Router/Standard.php
@@ -61,10 +61,7 @@ class Wsu_ComingSoon_Controller_Router_Standard extends Mage_Core_Controller_Var
 						$response->setHeader('Retry-After', '5000');
 						
 						if($coming_enabled==1){
-							$response->setRedirect(Mage::getBaseUrl() . 'index-coming.php');
-							$response->sendResponse();
-							$response->sendHeaders();
-							exit();
+							$maintenancePage=file_get_contents(Mage::getBaseUrl() . 'index-coming.php');
 						}
 						$response->setBody($maintenancePage);
 						$response->sendHeaders();
